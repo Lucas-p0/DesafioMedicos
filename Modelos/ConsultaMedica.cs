abe18bb4f6a4b65da6443e346fb5f3c18adf0559
Lucas Pinheiro <lucaslogos@gmail.com>
2023-12-18 16:28:34 -0300

    update

--- Modelos/ConsultaMedica.cs
+++ Modelos/ConsultaMedica.cs
@@ -1,41 +1,71 @@
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
 namespace DesafioMedico.Modelos;
 
 public class ConsultaMedica
 {
-    public DateTime DataConsulta { get; protected set; }
-    public string HoraDaConsulta { get; protected set; }
-    public string NomePaciente { get; protected set; }
-    public string? NumeroTelefone { get; protected set; }
-    public long Cpf { get; protected set; }
-    public string Rua { get; protected set; }
-    public string Cidade { get; protected set; }
-    public string Estado { get; protected set; }
-    public string Especialidade { get; protected set; }
-    public string NomeMedico { get; protected set; }
-    public bool Particular { get; protected set; }
-    public long NumeroDaCarteirinha { get; protected set; }
+    [JsonPropertyName("DataConsulta")]
+    public string? DataConsulta { get; protected set; }
 
-    public ConsultaMedica(DateTime dataConsulta, string horaConsulta, string nomePaciente, long cpf, string rua, string cidade, string estado, string especialidade, string nomeMedico, bool particular, long numeroDaCarteirinha)
-    {
-        SetDataConsulta(dataConsulta);
-        HoraDaConsulta = horaConsulta;
-        NomePaciente = nomePaciente;
-        Cpf = cpf;
-        Rua = rua;
-        Cidade = cidade;
-        Estado = estado;
-        Especialidade = especialidade;
-        NomeMedico = nomeMedico;
-        Particular = particular;
-        NumeroDaCarteirinha = numeroDaCarteirinha;
-    }
-    public void SetDataConsulta(DateTime dataConsulta)
-    {
-        if (dataConsulta < DateTime.Now)
-        {
-            System.Console.WriteLine($"A data da consulta deve ser menor que a data {dataConsulta}");
-        }
-        DataConsulta = dataConsulta;
-    }
+    [JsonPropertyName("HoraDaConsulta")]
+    public string? HoraDaConsulta { get; protected set; }
+
+    [JsonPropertyName("NomePaciente")]
+    public string? NomePaciente { get; protected set; }
+
+    [JsonPropertyName("NumeroTelefone")]
+    public string? NumeroTelefone { get; protected set; }
+
+    [JsonPropertyName("Cpf")]
+    public string Cpf { get; protected set; }
+
+    [JsonPropertyName("Rua")]
+    public string? Rua { get; protected set; }
+
+    [JsonPropertyName("Cidade")]
+    public string? Cidade { get; protected set; }
+
+    [JsonPropertyName("Estado")]
+    public string? Estado { get; protected set; }
+
+    [JsonPropertyName("Especialidade")]
+    public string? Especialidade { get; protected set; }
+
+    [JsonPropertyName("NomeMedico")]
+    public string? NomeMedico { get; protected set; }
+
+    [JsonPropertyName("Particular")]
+    public string? Particular { get; protected set; }
+
+    [JsonPropertyName("NumeroDaCarteirinha")]
+    public string? NumeroDaCarteirinha { get; protected set; }
+
+    [JsonPropertyName("ValorDaConsulta")]
+    public string? ValorDaConsulta { get; protected set; }
+
+
+    // public ConsultaMedica(DateTime dataConsulta, string horaConsulta, string nomePaciente, long cpf, string rua, string cidade, string estado, string especialidade, string nomeMedico, bool particular, long numeroDaCarteirinha)
+    // {
+    //     SetDataConsulta(dataConsulta);
+    //     HoraDaConsulta = horaConsulta;
+    //     NomePaciente = nomePaciente;
+    //     Cpf = cpf;
+    //     Rua = rua;
+    //     Cidade = cidade;
+    //     Estado = estado;
+    //     Especialidade = especialidade;
+    //     NomeMedico = nomeMedico;
+    //     Particular = particular;
+    //     NumeroDaCarteirinha = numeroDaCarteirinha;
+    // }
+    // public void SetDataConsulta(DateTime dataConsulta)
+    // {
+    //     if (dataConsulta < DateTime.Now)
+    //     {
+    //         System.Console.WriteLine($"A data da consulta deve ser menor que a data {dataConsulta}");
+    //     }
+    //     DataConsulta = dataConsulta;
+    // }
 }
 
--- obj/Debug/net6.0/DesafioMedico.AssemblyInfo.cs
+++ obj/Debug/net6.0/DesafioMedico.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesafioMedico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c5a52f5f84279d04fe7be551a4c63abdf6c7f39")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesafioMedico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesafioMedico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
